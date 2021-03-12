8b32e73fb5f49a0f52630852cd32b349351e9d0a
Abubakar Aftab <abubakar.aftab786@gmail.com>
2021-03-12 13:06:21 +0530

    Added Buttons to Cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -13,6 +13,9 @@ class CartItem extends React.Component{
                     <div style={{color:'#777'}} >Qty:1</div>
                     <div className="cart-item-actions">
                         {/*Buttons*/}
+                        <img alt="Add" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1615535381~hmac=b27509289ba3bfdd15c31e1c42be0f39" />
+                        <img alt="Remove" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1615535339~hmac=6ed6edfdcec9b977b93b9494c0477d13" />
+                        <img alt="Delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1615535400~hmac=93e37fcad67c2429b76526bd95087641" />
 
                     </div>
                 </div>
