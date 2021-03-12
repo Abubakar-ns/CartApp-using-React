3d7d8f67dccc747777a4cba0f24dfb705759cacb
Abubakar Aftab <abubakar.aftab786@gmail.com>
2021-03-12 12:59:07 +0530

    Make a Cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
+import React from 'react';
+import CartItem from './CartItem';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <h1>
-          Cart
-        </h1>
-        
+        <CartItem/>
       </header>
     </div>
   );
