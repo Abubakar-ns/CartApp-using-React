c4d7a4937b1ca9dfe6e2fe0f94070d623a07a89b
Abubakar Aftab <abubakar.aftab786@gmail.com>
2021-03-12 13:28:44 +0530

    Add increase quantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,7 +9,12 @@ class CartItem extends React.Component{
             qty: 1,
             img:''
         }
+        //this.increaseQuantity= this.increaseQuantity.bind(this);
     }
+    increaseQuantity=()=>{
+        console.log('this.state.price',this.state.price);
+    }
+
     render(){
     const { price,title,qty}= this.state; {/*object destructuring (I want these properties from above constructor)*/}
         return(
@@ -23,9 +28,23 @@ class CartItem extends React.Component{
                     <div style={{color:'#777'}} >{qty}</div>
                     <div className="cart-item-actions">
                         {/*Buttons*/}
-                        <img alt="Add" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1615535381~hmac=b27509289ba3bfdd15c31e1c42be0f39" />
-                        <img alt="Remove" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1615535339~hmac=6ed6edfdcec9b977b93b9494c0477d13" />
-                        <img alt="Delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1615535400~hmac=93e37fcad67c2429b76526bd95087641" />
+                        <img 
+                            alt="Add" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1615535381~hmac=b27509289ba3bfdd15c31e1c42be0f39"
+                            onClick={this.increaseQuantity}
+                         />
+                        <img 
+                            alt="Remove" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1615535339~hmac=6ed6edfdcec9b977b93b9494c0477d13" 
+                            
+                        />
+                        <img 
+                            alt="Delete" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1615535400~hmac=93e37fcad67c2429b76526bd95087641" 
+                        />
 
                     </div>
                 </div>
